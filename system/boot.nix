f8c0c1ea6f16c19df7cfc1065e80648942b5c9e2
Poppy - <me@poppyio.com>
2024-07-22 15:29:01 +0100

    kernelParams typo fix

--- system/boot.nix
+++ system/boot.nix
@@ -15,7 +15,7 @@
       "quiet"
       "splash"
       "boot.shell_on_fail"
-      "loglevel = 3"
+      "loglevel=3"
       "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
       "udev.log_priority=3"
