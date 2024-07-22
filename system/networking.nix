c20f64ba906596a9887e60088aa3266aee117703
Poppy - <me@poppyio.com>
2024-07-22 03:01:04 +0100

    🧱 continue flaking system pkgs

---
+++ system/networking.nix
@@ -0,0 +1,10 @@
+{...}: {
+  networking.hostName = "poppy-nix"; # Define your hostname.
+  networking.nameservers = [
+    "1.1.1.1"
+    "9.9.9.9"
+  ];
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+}
