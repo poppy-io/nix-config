6367b5fd12f4b3c50897320893406beb34a9d48a
Poppy - <me@poppyio.com>
2024-07-22 20:31:45 +0100

    bugfix of last commit

--- system/vscode.nix
+++ system/vscode.nix
@@ -1,29 +1,31 @@
 {pkgs, ...}: {
   environment.systemPackages = with pkgs; [
-    vscode = vscodium;
-    vscodeExtensions = with vscode-extensions; [
-      jnoortheen.nix-ide
-      ms-python.python
-      ms-python.isort
-      ms-python.debugpy
-      ms-python.black-formatter
-      ms-pyright.pyright
-      yzhang.markdown-all-in-one
-      wakatime.vscode-wakatime
-      vscode-icons-team.vscode-icons
-      vscjava.vscode-java-pack
-      uloco.theme-bluloco-light
-      # tuttieee.emacs-mcx
-      tomoki1207.pdf
-      timonwong.shellcheck
-      tamasfe.even-better-toml
-      shardulm94.trailing-spaces
-      serayuzgur.crates
-      seatonjiang.gitmoji-vscode
-      scala-lang.scala
-      njpwerner.autodocstring
-      mattn.lisp
-      mathiasfrohlich.kotlin
-    ];
+    (vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        jnoortheen.nix-ide
+        ms-python.python
+        ms-python.isort
+        ms-python.debugpy
+        ms-python.black-formatter
+        ms-pyright.pyright
+        yzhang.markdown-all-in-one
+        wakatime.vscode-wakatime
+        vscode-icons-team.vscode-icons
+        vscjava.vscode-java-pack
+        uloco.theme-bluloco-light
+        # tuttieee.emacs-mcx
+        tomoki1207.pdf
+        timonwong.shellcheck
+        tamasfe.even-better-toml
+        shardulm94.trailing-spaces
+        serayuzgur.crates
+        seatonjiang.gitmoji-vscode
+        scala-lang.scala
+        njpwerner.autodocstring
+        mattn.lisp
+        mathiasfrohlich.kotlin
+      ];
+    });
   ];
 }
