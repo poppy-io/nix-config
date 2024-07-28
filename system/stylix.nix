456f6f488c75fe94f7e2b1bc43ad825813455f95
Poppy - <me@poppyio.com>
2024-07-28 16:51:54 +0100

    change theme yet again

--- system/stylix.nix
+++ system/stylix.nix
@@ -13,7 +13,7 @@
       sha256 = "4ehXDzIBPCkqO5u0zNNaWsgvM0gR9qZP8SbKYAjdOas=";
     };
     polarity = "light";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/da-one-paper.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
 
     opacity = {
       applications = 0.7;
