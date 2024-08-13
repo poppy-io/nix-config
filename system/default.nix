c56ac87677e67fb45821204dcea9b57b882f760d
Poppy - <me@poppyio.com>
2024-08-13 17:13:14 +0100

    continue work on xdg portal

--- system/default.nix
+++ system/default.nix
@@ -35,6 +35,8 @@
     useXkbConfig = true; # use xkb.options in tty.
   };
 
+  xdg.portal.enable = true;
+  xdg.portal.xdgOpenUsePortal = true;
   xdg.portal.extraPortals = with pkgs; [
     xdg-desktop-portal-wlr
     xdg-desktop-portal-gtk
