1a112747dda997443e4b4e4c89d29333e32eba97
Poppy - <me@poppyio.com>
2024-08-20 16:27:44 +0100

    remove hangul support due to build issues

--- system/i18n/ko.nix
+++ system/i18n/ko.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   i18n = {
     supportedLocales = ["ko_KR.UTF-8/UTF-8"];
-    inputMethod.ibus.engines = with pkgs.ibus-engines; [hangul];
+    # inputMethod.ibus.engines = with pkgs.ibus-engines; [hangul];
   };
 }
