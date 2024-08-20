a1c1d7a5784a75839599b8ba852e44d1d4b3938b
Poppy - <me@poppyio.com>
2024-08-20 18:45:29 +0100

    switch back to fcitx5

--- system/i18n/default.nix
+++ system/i18n/default.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./de.nix
     ./zh.nix
@@ -8,12 +8,20 @@
 
   i18n = {
     defaultLocale = "en_GB.UTF-8/UTF-8";
-    inputMethod.enable = true;
-    inputMethod.type = "ibus";
+
+    inputMethod = {
+      type = "fcitx5";
+      enable = true;
+      fcitx5 = {
+        waylandFrontend = true;
+        addons = with pkgs; [
+          fcitx5-gtk
+          fcitx5-rose-pine # colour theme; consider also fcitx5-nord or fcitx5-material-color
+        ];
+      };
+    };
   };
 
-  environment.variables.GLFW_IM_MODULE = "ibus";
-
-  # use equivalent BAC timezone due to lack of Scottish option
+  # use equivalent BÁC timezone due to lack of Scottish option
   time.timeZone = "Europe/Dublin";
 }
