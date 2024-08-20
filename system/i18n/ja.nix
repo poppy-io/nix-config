691dc41d57fb805bbbbe20e6646ed5cda1a7eccc
Poppy - <me@poppyio.com>
2024-08-20 16:05:27 +0100

    ✏️ correct typo

--- system/i18n/ja.nix
+++ system/i18n/ja.nix
@@ -2,6 +2,6 @@
   i18n = {
     supportedLocales = ["ja_JP.UTF-8"];
 
-    i18n.inputMethod.ibus.engines = with pkgs.ibus-engines; [anthy];
+    inputMethod.ibus.engines = with pkgs.ibus-engines; [anthy];
   };
 }
