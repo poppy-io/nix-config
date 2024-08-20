2aed5fc8665927b717abcae5b98c873ef6d0308f
Poppy - <me@poppyio.com>
2024-08-20 16:26:25 +0100

    add hangul support; swap anthy for mozc

--- system/i18n/ja.nix
+++ system/i18n/ja.nix
@@ -2,6 +2,6 @@
   i18n = {
     supportedLocales = ["ja_JP.UTF-8/UTF-8"];
 
-    inputMethod.ibus.engines = with pkgs.ibus-engines; [anthy];
+    inputMethod.ibus.engines = with pkgs.ibus-engines; [mozc];
   };
 }
