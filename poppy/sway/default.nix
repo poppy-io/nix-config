80bc13edcb1e29be531dda6b55fe8e49b03e7f39
Poppy - <me@poppyio.com>
2024-08-22 22:56:40 +0100

    upgrade i3status to i3status-rust and manually configure

--- poppy/sway/default.nix
+++ poppy/sway/default.nix
@@ -15,6 +15,13 @@
       terminal = "kitty";
       startup = [{command = "vesktop";}];
       window.titlebar = false;
+
+      bars = [
+        {
+          position = "top";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
+        }
+      ];
     };
   };
 
@@ -26,4 +33,39 @@
   home.packages = with pkgs; [
     clipman
   ];
+
+  # information for swaybar
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      top = {
+        icons = "material-nf";
+        theme = "ctp-frappe";
+        blocks = [
+          {
+            block = "time";
+            interval = 60;
+            format = "%a %d/%m %k:%M %p";
+          }
+          {
+            block = "cpu";
+          }
+          {
+            block = "memory";
+            format = " $icon $mem_total_used_percents.eng(w:2) ";
+            format_alt = " $icon_swap $swap_used_percents.eng(w:2) ";
+          }
+          {
+            block = "disk_space";
+            path = "/";
+            info_type = "available";
+            interval = 20;
+            warning = 20.0;
+            alert = 10.0;
+            format = " $icon root: $available.eng(w:2) ";
+          }
+        ];
+      };
+    };
+  };
 }
