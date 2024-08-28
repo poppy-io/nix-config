039a416118184e77145fd08927e63b2c1d6886e6
Poppy - <me@poppyio.com>
2024-08-28 04:36:26 +0100

    add agl

--- system/games/hk3/default.nix
+++ system/games/hk3/default.nix
@@ -1,9 +1,11 @@
-{ config, pkgs, lib, inputs, ... }:
-let
+{
+  lib,
+  inputs,
+  ...
+}: let
   customIcon = builtins.fetchurl (lib.importJSON ./icon.json);
   aaglPkgs = inputs.aagl.packages.x86_64-linux;
-in
-{
+in {
   programs.honkers-launcher = {
     enable = true;
     package = aaglPkgs.honkers-launcher.override (old: {
