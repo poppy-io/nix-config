0d8929c369f7093f090408043c364677b04312e3
Poppy - <me@poppyio.com>
2024-08-28 04:38:58 +0100

    continuation of previous commit

---
+++ system/games/agl/default.nix
@@ -0,0 +1,17 @@
+{
+  lib,
+  inputs,
+  ...
+}: let
+  customIcon = builtins.fetchurl (lib.importJSON ./icon.json);
+  aaglPkgs = inputs.aagl.packages.x86_64-linux;
+in {
+  programs.honkers-launcher = {
+    enable = true;
+    package = aaglPkgs.an-anime-game-launcher.override (old: {
+      unwrapped = old.unwrapped.override {
+        inherit customIcon;
+      };
+    });
+  };
+}
