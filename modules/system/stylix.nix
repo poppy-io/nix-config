cb867f37182db4b830c4cf1f098714fc5aa640ce
Poppy - <me@poppyio.com>
2024-10-10 13:14:33 +0100

    update some theming

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -9,11 +9,11 @@
     enable = true;
 
     image = pkgs.fetchurl {
-      url = "https://img3.gelbooru.com/images/19/9e/199ead05a0eee4d85d4f65076a379a28.jpg";
-      sha256 = "4ehXDzIBPCkqO5u0zNNaWsgvM0gR9qZP8SbKYAjdOas=";
+      url = "https://64.media.tumblr.com/6311cba4718459408cba35d6e676a80e/tumblr_n2oztnaz4W1suezzpo1_500.jpg";
+      sha256 = "0i6hv60sj0lg42kcm6b02kvih011ibk34mxsxyyigjh6143lbhr3";
     };
     polarity = "light";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/danqing.yaml";
 
     opacity = {
       applications = 0.7;
@@ -48,9 +48,9 @@
       };
     };
 
-    #cursor = {
-    #  package = pkgs.hackneyed;
-    #  name = "Hackneyed Dark";
-    #};
+    cursor = {
+      package = pkgs.hackneyed;
+      name = "Hackneyed Dark";
+    };
   };
 }
