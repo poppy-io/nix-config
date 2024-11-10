6c3eae363e61c6e1034943ffe098b11fdb8ad59f
Poppy - <me@poppyio.com>
2024-11-10 19:19:35 +0000

    finish samba config for now

--- modules/system/samba.nix
+++ modules/system/samba.nix
@@ -1,16 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   environment.systemPackages = with pkgs; [cifs-utils];
   fileSystems."/mnt/share/music" = {
-    device = "//EXENEBOX/Music";
+    device = "//192.168.0.27/Music";
     fsType = "cifs";
 
     options = let
       # prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts}"];
+    in ["${automount_opts},uid=${toString config.users.users.poppy.uid}"];
   };
 
   services.gvfs.enable = true;
 
   networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
 }
+# 192.168.0.27
+
