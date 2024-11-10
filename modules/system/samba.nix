1601830ba944f71608c5ca68172734f856fc872a
Poppy - <me@poppyio.com>
2024-11-10 22:36:07 +0000

    configure mpd

--- modules/system/samba.nix
+++ modules/system/samba.nix
@@ -12,11 +12,10 @@
       # prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
     in ["${automount_opts},uid=${toString config.users.users.poppy.uid}"];
+    # FIXME: root still required to access
   };
 
   services.gvfs.enable = true;
 
   networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
 }
-# 192.168.0.27
-
---
+++ modules/user/music/default.nix
@@ -0,0 +1,6 @@
+{
+  imports = [
+    ./spotify.nix
+    ./rmpc.nix
+  ];
+}
