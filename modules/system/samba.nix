752e7e7ca56f8c7d37d80062fd9121d2d82c878b
Poppy - <me@poppyio.com>
2024-11-21 18:08:55 +0000

    finish samba config; update cursor; switch to building lix from HEAD

--- modules/system/samba.nix
+++ modules/system/samba.nix
@@ -22,6 +22,16 @@
     # FIXME: hardcoded uid
   };
 
+  fileSystems."/mnt/share/network-share" = {
+    device = "//192.168.0.27/Music-staging";
+    fsType = "cifs";
+
+    options = let
+      # prevents hanging on network split
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},uid=1000,gid=1,rw"];
+  };
+
   services.gvfs.enable = true;
 
   networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
