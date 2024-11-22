98cb751000e8e1030cd834883a6796511590306f
Poppy - <me@poppyio.com>
2024-11-22 23:15:12 +0000

    add xcb flag to flameshot keybinds

--- modules/user/wm/sway/keybinds.nix
+++ modules/user/wm/sway/keybinds.nix
@@ -9,13 +9,13 @@
   in
     lib.mkOptionDefault {
       ## Screenshot
-      "Print" = "exec ${flameshot} gui -c -p ${xdg-user-dir}"; # Manually select a region
-      "Shift+Print" = "exec ${flameshot} screen -c -p ${xdg-user-dir}"; # Current screen
-      "Alt+Print" = "exec ${flameshot} full -c -p ${xdg-user-dir}"; # Full screen
-      "Shift+Alt+Print" = "exec ${flameshot} screen -n 1 -c -p ${xdg-user-dir}"; # Screen 1
-      "Ctrl+Print" = "exec ${flameshot} gui -c -p ${xdg-user-dir} -d 5000"; #Manually select a region with 5s delay
-      "Ctrl+Shift+Print" = "exec ${flameshot} screen -c -p ${xdg-user-dir} -d 5000"; # Current screen with 5s delay
-      "Ctrl+Alt+Print" = "exec ${flameshot} full -c -p ${xdg-user-dir} -d 5000"; # Full screen with 5s delay
-      "Ctrl+Shift+Alt+Print" = "exec ${flameshot} screen -n 1 -c -p ${xdg-user-dir} -d 5000"; # Screen 1 with 5s delay
+      "Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} gui -c -p ${xdg-user-dir}"; # Manually select a region
+      "Shift+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} screen -c -p ${xdg-user-dir}"; # Current screen
+      "Alt+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} full -c -p ${xdg-user-dir}"; # Full screen
+      "Shift+Alt+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} screen -n 1 -c -p ${xdg-user-dir}"; # Screen 1
+      "Ctrl+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} gui -c -p ${xdg-user-dir} -d 5000"; #Manually select a region with 5s delay
+      "Ctrl+Shift+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} screen -c -p ${xdg-user-dir} -d 5000"; # Current screen with 5s delay
+      "Ctrl+Alt+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} full -c -p ${xdg-user-dir} -d 5000"; # Full screen with 5s delay
+      "Ctrl+Shift+Alt+Print" = "exec QT_QPA_PLATFORM=xcb ${flameshot} screen -n 1 -c -p ${xdg-user-dir} -d 5000"; # Screen 1 with 5s delay
     };
 }
