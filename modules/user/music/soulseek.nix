9678cb58c5b57d9110189534390034b31506a131
Poppy - <me@poppyio.com>
2025-01-05 11:59:13 +0000

    temp disable flacon; update code

--- modules/user/music/soulseek.nix
+++ modules/user/music/soulseek.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
   # use proprietary client cause nicotine crashes nonstop :(
-  home.packages = with pkgs; [soulseekqt flacon];
+  home.packages = with pkgs; [
+    soulseekqt
+    #flacon  temp disable due to build issues
+  ];
 }
